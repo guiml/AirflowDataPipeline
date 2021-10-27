fd6c096e57798171245b670c6d451bda58d8a43b
Guilherme Mussoi Louzada <guiml@users.noreply.github.com>
2021-10-26 20:30:17 -0600

    Update dag.py
    Removed restriction of songs folders for full execution

--- dags/dag.py
+++ dags/dag.py
@@ -46,7 +46,7 @@ stage_songs_to_redshift = StageToRedshiftOperator(
     aws_credentials_id='aws_credentials',
     table='staging_songs',
     s3_bucket='udacity-dend',
-    s3_key='song_data/A/A',
+    s3_key='song_data',
     region = 'us-west-2',
     param="FORMAT AS JSON 'auto'" 
 )
